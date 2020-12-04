8319f9c3dfd273d8b9c17a3b48efab0eed89a89e
MrG3P5 <mrg3p5@gmail.com>
2020-12-05 01:46:12 +0700

    nih yang pm2 error

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 const pm2 = require('pm2')
 const cron = require('node-cron');
 const fs = require('fs');
-const settings = JSON.parse(fs.readFileSync('./lib/setting.json'))
+const settings = JSON.parse(fs.readFileSync('./lib/database/setting.json'))
 
 pm2.connect((error) => {
     if (error) {
@@ -18,7 +18,7 @@ pm2.connect((error) => {
     if(settings.Rest){
       cron.schedule("50 59 8 * * *", function(){
         settings.banChats = true
-        fs.writeFileSync('./lib/setting.json',JSON.stringify(settings,null,2))
+        fs.writeFileSync('./lib/database/setting.json',JSON.stringify(settings,null,2))
         pm2.restart('index', (error) => {
           if (error) {
             console.error(error)
@@ -29,7 +29,7 @@ pm2.connect((error) => {
   
       cron.schedule("0 0 9 * * *", function(){
         settings.banChats = false
-        // fs.writeFileSync('./lib/setting.json',JSON.stringify(settings,null,2))
+        // fs.writeFileSync('./lib/database/setting.json',JSON.stringify(settings,null,2))
         pm2.restart('index', (error) => {
           if (error) {
             console.error(error)
@@ -41,7 +41,7 @@ pm2.connect((error) => {
     
     cron.schedule("0 0 0 * * *", function(){
       let obj = [{id: settings.owner, limit: 1}];
-      fs.writeFileSync('./lib/limit.json', JSON.stringify(obj));
+      fs.writeFileSync('./lib/database/limit.json', JSON.stringify(obj));
       pm2.restart('index', (error) => {
         if (error) {
           console.error(error)
