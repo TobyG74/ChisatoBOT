bcd733c67cc6b07baceec5510dbfad57eb4bb206
TobyG74 <tobisaputra11@gmail.com>
2024-01-25 17:33:09 +0700

    feat: added several new features

--- src/commands/downloader/facebookVideo.ts
+++ src/commands/downloader/facebookVideo.ts
@@ -13,10 +13,20 @@ export default <ConfigCommands>{
     example: `• /fbvideo https://web.facebook.com/watch?v=xxxxxxx`,
     async run({ Chisato, from, query, message, command }) {
         if (!isURL(query)) return Chisato.sendText(from, "Please input a valid url!", message);
-        downloader.facebookVideo(query).then((res) => {
-            if (res.result.length === 0) return Chisato.sendText(from, "Video not found!", message);
-            const caption = `*「 FACEBOOK VIDEO DOWNLOADER 」*\n\n• Duration: ${res.duration}\n• Quality: ${res.result[0].quality}`;
-            Chisato.sendVideo(from, res.result[0].url, false, caption, message);
-        });
+        downloader
+            .facebookVideo(query)
+            .then((res) => {
+                if (res.result.length === 0) return Chisato.sendText(from, "Video not found!", message);
+                const caption = `*「 FACEBOOK VIDEO DOWNLOADER 」*\n\n• Duration: ${res.duration}\n• Quality: ${res.result[0].quality}`;
+                Chisato.sendVideo(from, res.result[0].url, false, caption, message);
+            })
+            .catch((e) => {
+                Chisato.log("error", command.name, e);
+                Chisato.sendText(
+                    from,
+                    "There is an error. Please report it to the bot creator immediately!\nMessage : " + e,
+                    message
+                );
+            });
     },
 };
