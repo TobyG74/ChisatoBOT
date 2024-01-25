46f816f5ea82c7b1353a67dcd610df5cf14c38e8
TobyG74 <tobisaputra11@gmail.com>
2024-01-25 17:19:59 +0700

    feat: add GroupSetting to statistics

--- src/commands/misc/statistics.ts
+++ src/commands/misc/statistics.ts
@@ -3,16 +3,16 @@ import path from "path";
 import fs from "fs";
 
 /** Database */
-import { Group } from "../../libs/database/group";
-import { User } from "../../libs/database/user";
+import { Group, User, GroupSetting } from "../../libs/database";
 
 export default <ConfigCommands>{
     name: "stats",
-    alias: ["statistics"],
+    alias: ["statistics", "statistic", "stat"],
     category: "misc",
     description: "View the count from the database",
     async run({ Chisato, from, message }) {
         const group = new Group();
+        const groupSetting = new GroupSetting();
         const user = new User();
         const filepath = path.join(__dirname, "../../..", `temp`);
         const files = fs.readdirSync(filepath);
@@ -20,6 +20,7 @@ export default <ConfigCommands>{
         const caption =
             `「 *TOTAL DATABASE* 」\n\n` +
             `• Group : ${await group.size()}\n` +
+            `• Group Setting : ${await groupSetting.size()}\n` +
             `• User : ${await user.size()}\n` +
             `• AI User : ${files.length}\n`;
 
