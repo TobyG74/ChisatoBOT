fb04eda2af22d7ebd012d16514245a8fd3ba5fbc
TobyG74 <tobisaputra11@gmail.com>
2024-01-25 17:19:35 +0700

    fix: database not updated

--- src/commands/group/leave.ts
+++ src/commands/group/leave.ts
@@ -8,8 +8,10 @@ export default <ConfigCommands>{
     description: "Leave from group",
     isGroup: true,
     isGroupAdmin: true,
-    async run({ Chisato, from, message }) {
+    async run({ Chisato, from, message, Database }) {
         await Chisato.sendText(from, "Bye All ~", message);
-        Chisato.groupLeave(from);
+        Chisato.groupLeave(from).then(() => {
+            Database.Group.delete(from);
+        });
     },
 };
