dfa65aa21e7eebd68f8b4ee230af3c55a2bd187e
TobyG74 <tobisaputra11@gmail.com>
2024-01-29 22:55:04 +0700

    fix: superadmin not counted in admin count

--- src/commands/group/groupInformation.ts
+++ src/commands/group/groupInformation.ts
@@ -8,31 +8,35 @@ export default <ConfigCommands>{
     isGroup: true,
     async run({ Chisato, from, message }) {
         const groupMetadata = await Chisato.groupMetadata(from);
+        const groupAdmins = groupMetadata.participants.filter((v) => v.admin !== null);
         let str =
             `*「 GROUP INFORMATION 」*\n\n` +
             `• Name : ${groupMetadata.subject}\n` +
             `• Description : ${groupMetadata.desc || "-"}\n` +
-            `• Owner : @${groupMetadata.owner.split("@")[0]}\n` +
             `• Member Count : ${groupMetadata.participants.length}\n` +
+            `• Group Owner : @${groupMetadata.owner.split("@")[0]}\n` +
             `• Group Owner : ${groupMetadata.owner || "-"}\n` +
-            `• Group Admins : ${groupMetadata.participants.filter((v) => v.admin === "admin").length}\n` +
-            `• Group Created : ${new Date(groupMetadata.creation * 1000).toLocaleString("en-US", {
-                weekday: "long",
-                day: "numeric",
-                month: "long",
-                year: "numeric",
-                hour: "numeric",
-                minute: "numeric",
-                second: "numeric",
-            })}\n`;
+            `• Group Admins : ${groupAdmins.length}\n`;
+        for (let i = 0; i < groupAdmins.length; i++) {
+            str += `└ @${groupAdmins[i].id.split("@")[0]}\n`;
+        }
+        str += `• Group Created : ${new Date(groupMetadata.creation * 1000).toLocaleString("en-US", {
+            weekday: "long",
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+        })}\n`;
         try {
             const picture = await Chisato.profilePictureUrl(from, "image");
             await Chisato.sendImage(from, picture, str, message, {
-                mentions: [groupMetadata.owner],
+                mentions: groupAdmins.map((v) => v.id),
             });
         } catch {
             await Chisato.sendText(from, str, message, {
-                mentions: [groupMetadata.owner],
+                mentions: groupAdmins.map((v) => v.id),
             });
         }
     },
